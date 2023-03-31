9eb2776d1cddb0b9510e7510b3431a6001b1956b
Chris Malloy <cjmalloy@gmail.com>
2023-03-31 10:58:15 -0300

    Fixed quotes

--- 40-create-nginx-config.sh
+++ 40-create-nginx-config.sh
@@ -6,12 +6,12 @@ server {
     listen  [::]:8022;
     server_name  localhost;
 
-    proxy_set_header User-Tag "${USER_TAG}";
-    proxy_set_header Local-Origin "${LOCAL_ORIGIN}";
-    proxy_set_header Read-Access "${READ_ACCESS}";
-    proxy_set_header Write-Access "${WRITE_ACCESS}";
-    proxy_set_header Tag-Read-Access "${TAG_READ_ACCESS}";
-    proxy_set_header Tag-Write-Access "${TAG_WRITE_ACCESS}";
+    proxy_set_header User-Tag \"${USER_TAG}\";
+    proxy_set_header Local-Origin \"${LOCAL_ORIGIN}\";
+    proxy_set_header Read-Access \"${READ_ACCESS}\";
+    proxy_set_header Write-Access \"${WRITE_ACCESS}\";
+    proxy_set_header Tag-Read-Access \"${TAG_READ_ACCESS}\";
+    proxy_set_header Tag-Write-Access \"${TAG_WRITE_ACCESS}\";
 
     location / {
         proxy_pass ${UPSTREAM-http://localhost:8081/}
